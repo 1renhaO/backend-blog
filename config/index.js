0e0e5cdc944d6f237f3476fe5a0da0698538265e
liurenhao <cqlplrh@hotmail.com>
2018-02-27 17:50:34 +0800

    fixed wechat check

--- config/index.js
+++ config/index.js
@@ -1,4 +1,4 @@
 module.exports = {
     port: process.env.NODE_ENV === 'develop' ? 8090 : 8081,
-    wecahtToken: process.env.WECHAT_TOKEN
+    wechatToken: process.env.WECHAT_TOKEN
 }
--- router/wechat.js
+++ router/wechat.js
@@ -1,9 +1,8 @@
 const Router = require('koa-router')
-let wechatToken = require('../config').wecahtToken
+let wechatToken = require('../config').wechatToken
 let sha1 = require('sha1')
 let keys = ['timestamp', 'nonce']
 let wechatRouter = new Router()
-const fs = require('fs')
 wechatRouter.get('/wechat', async (ctx, next) => {
     let arr = []
     for (key in ctx.request.query) {
@@ -11,20 +10,10 @@ wechatRouter.get('/wechat', async (ctx, next) => {
         arr.push(ctx.request.query[key])
     }
     arr.push(wechatToken)
-    let _signature = sha1(arr.sort().join())
-    if (_signature === ctx.request.query.signature) {
+    if (sha1(arr.sort().join('')) === ctx.request.query.signature) {
         ctx.response.body = ctx.request.query.echostr
     } else {
-        let response = {
-            _signature,
-            signature: ctx.request.query.signature,
-            'echostr': ctx.request.query.echostr,
-            'timestamp': ctx.request.query.timestamp,
-            'nonce': ctx.request.query.nonce,
-            'token': wechatToken
-        }
-        fs.writeFile('fail.txt', JSON.stringify(response), (err) => { })
-        ctx.response.body = response
+        ctx.response.body = 'fail'
     }
 })
 
