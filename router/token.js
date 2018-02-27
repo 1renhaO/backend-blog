529eca2011a9e88a4d53d79a99f640458258b103
liurenhao <cqlplrh@hotmail.com>
2018-02-27 23:48:58 +0800

    remove log file

--- router/token.js
+++ router/token.js
@@ -3,7 +3,6 @@ let wechatToken = require('../config').wechatToken
 let sha1 = require('sha1')
 let keys = ['timestamp', 'nonce']
 let tokenRouter = new Router()
-const fs = require('fs')
 tokenRouter.get('/token', async (ctx, next) => {
     let arr = []
     for (key in ctx.request.query) {
@@ -15,15 +14,6 @@ tokenRouter.get('/token', async (ctx, next) => {
     if (_signature === ctx.request.query.signature) {
         ctx.response.body = ctx.request.query.echostr
     } else {
-        fs.writeFile('fail.txt', JSON.stringify({
-            _signature,
-            join: arr.sort().join(''),
-            signature: ctx.request.query.signature,
-            nonce: ctx.request.query.nonce,
-            echostr: ctx.request.query.echostr,
-            timestamp: ctx.request.query.timestamp,
-            token: wechatToken
-        }), () => { })
         ctx.response.body = 'fail'
     }
 })
