f48ebd08b2bde6525d893d28a25222c538f56ce4
liurenhao <cqlplrh@hotmail.com>
2018-02-27 15:31:50 +0800

    fixed wechat token check

--- router/wechat.js
+++ router/wechat.js
@@ -1,7 +1,7 @@
 const Router = require('koa-router')
-let wechatToken = require('../config')
+let wechatToken = require('../config').wecahtToken
 let sha1 = require('sha1')
-let keys = ['timestamp', 'echostr', 'nonce']
+let keys = ['timestamp', 'nonce']
 let wechatRouter = new Router()
 wechatRouter.get('/wechat', async (ctx, next) => {
     let arr = []
@@ -9,8 +9,9 @@ wechatRouter.get('/wechat', async (ctx, next) => {
         if (!keys.includes(key)) continue
         arr.push(ctx.request.query[key])
     }
+    arr.push(wechatToken)
     if (sha1(arr.sort().join()) === ctx.request.query.signature) {
-        ctx.response.body = 'access'
+        ctx.response.body = ctx.request.query.echostr
     } else {
         ctx.response.body = 'fail'
     }
