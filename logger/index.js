c1df5687a6f0ca3ac69259e7e9903da8189888a7
liurenhao <renhao.liu@qingtui.cn>
2018-05-30 14:45:54 +0800

    fxied: log dir spelling mistake

--- logger/index.js
+++ logger/index.js
@@ -2,8 +2,8 @@ const log4js = require('log4js')
 const path = require('path')
 log4js.configure({
   appenders: {
-    accessLogger: { type: 'dateFile', filename: path.resolve(__dirname, '../app_log/log4js/access.log'), keepFileExt: true, daysToKeep: 30 },
-    errorLogger: { type: 'dateFile', filename: path.resolve(__dirname, '../app_log/log4js/error.log'), keepFileExt: true, daysToKeep: 30 }
+    accessLogger: { type: 'dateFile', filename: path.resolve(__dirname, '../app_logs/log4js/access.log'), keepFileExt: true, daysToKeep: 30 },
+    errorLogger: { type: 'dateFile', filename: path.resolve(__dirname, '../app_logs/log4js/error.log'), keepFileExt: true, daysToKeep: 30 }
   },
   categories: {
     default: { appenders: ['accessLogger'], level: 'trace' },
